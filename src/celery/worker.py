f3b557dfd792bc45f9a95268736f98a42514c7df
Refaelbenzvi24 <refaelbenzvi24@gmail.com>
2022-11-20 11:26:08 +0200

    feat(workers): added uuid to worker hostname to allow multi workers horizontal scaling

--- src/celery/worker.py
+++ src/celery/worker.py
@@ -1,13 +1,21 @@
-import uvicorn
+import uuid
+
 from ..config.celeryApp import celery_app
 
 celery_app.config_from_object(__name__)
 
 
 def main():
+	workers_map = celery_app.control.inspect().active()
+	workers_list = []
+	
+	if workers_map is not None:
+		workers_list = [*workers_map.keys()]
+	
 	worker = celery_app.Worker(
 		pool='solo',
-		hostname='celery@worker',
+		hostname=f'worker@{len(workers_list) + 1}@{uuid.uuid4()}',
+		queues=['default'],
 		loglevel='info',
 		task_events=True,
 		include=['src.shared.tasks.tasks']
